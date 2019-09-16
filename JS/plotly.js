7bd32aa7369c8a660ec6a01322681854bfb6ac05
Manoel Britto <manoelbritto@gmail.com>
2019-09-16 17:05:49 -0400

    including js function to extract data from json

--- JS/plotly.js
+++ JS/plotly.js
@@ -1,9 +1,12 @@
+//global variable
+var data;
+
 //init function to fill in the select option
+
 function init() {
   d3.json("JS/data/samples.json").then(dataInitial => {
-    var data = dataInitial;
-
-    selectValues = dataInitial.names;
+    data = dataInitial;
+    var selectValues = dataInitial.names;
 
     var selectOpt = d3.select("#selDataset");
 
@@ -17,4 +20,26 @@ function init() {
     });
   });
 }
+//start filling the data inside the select option
 init();
+
+//
+
+d3.selectAll("#selDataset").on("change", demographicFunc);
+
+function demographicFunc() {
+  var valueSelect = d3.select("#selDataset").node().value;
+  //   console.log(valueSelect);
+  var filterValue = data.metadata.filter(value => value.id == valueSelect);
+  console.log(filterValue);
+  console.log(filterValue[0].id);
+  var divValue = d3.select(".panel-body");
+  divValue.html("");
+  divValue.append("p").text(`id: ${filterValue[0].id}`);
+  divValue.append("p").text(`ethnicity: ${filterValue[0].ethnicity}`);
+  divValue.append("p").text(`gender: ${filterValue[0].gender}`);
+  divValue.append("p").text(`age: ${filterValue[0].age}`);
+  divValue.append("p").text(`location: ${filterValue[0].location}`);
+  divValue.append("p").text(`bbtype: ${filterValue[0].bbtype}`);
+  divValue.append("p").text(`wfreq: ${filterValue[0].wfreq}`);
+}
